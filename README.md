feda27cce758c498c938d89327ad56b65d8641e9
Favour Samuel <sfchidimma@gmail.com>
2021-08-04 15:01:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Vertical Slider
+Project #26 in Brad Traversy's 50 Projects in 50 Days. This is a vertical slider. Clicking the arrows changes the images and content. This project is not responsive.
+
+![Vertical Slider](screenshot.jpg)
+
+## Built With
+- [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+- [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Install
+```
+    $ git clone git@github.com:samiiexx/sam-js-26.git
+    $ cd main
+```  
+## Contributors
+- Favour Samuel (@samiiexx)
+
+## License
+Copyright (c) 2021 Favour Samuel.
+
+For enquiries please contact me at [sfchidimma@gmail.com](mailto:sfchidimma@gmail.com).
---
+++ index.js
@@ -0,0 +1,48 @@
+// Copyright 2021 sfchi
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide ('up'))
+downButton.addEventListener('click', () =>  changeSlide ('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    }
+    else if (direction === 'down') {
+        activeSlideIndex--
+
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(+${activeSlideIndex * sliderHeight}px)`
+}
